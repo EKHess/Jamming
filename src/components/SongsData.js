21bd0cd74c9937877b032d44d1667b607f95660f
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-03-02 10:28:52 -0700

    Created mock data in SongsData.js

---
+++ src/components/SongsData.js
@@ -0,0 +1,16 @@
+const tracks = [
+    {
+        title: "Take On Me",
+        artist: "A-ha",
+    },
+    {
+        title: "Sweet Dreams (Are Made of This) - Remastered",
+        artist: "Eurythmics, Annie Lennox, Dave Stewart",
+    },
+    {
+        title: "You Make My Dreams (Come True)",
+        artist: "Daryl Hall & John Oates",
+    }
+]
+
+export default tracks;
