721ab1339adfec2270a3e269f7024d0a11d52a14
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-03-02 10:15:10 -0700

    Made a Track component

---
+++ src/components/Track.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Track() {
+    return (
+        <>
+            <p>Artist Name</p>
+            <button>Add</button>
+        </>
+    )
+}
+
+export default Track;
