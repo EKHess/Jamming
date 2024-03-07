ea9c79423e694f2647b20e4a81a347355a2aff2a
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-03-07 11:31:51 -0700

    Added Sptify URIs to mock data in SongsData.js

--- src/SongsData.js
+++ src/SongsData.js
@@ -6,36 +6,42 @@ const tracks = [
         artist: "A-ha",
         album: "Hunting High and Low",
         id: generateId(),
+        uri: 'spotify:track:2WfaOiMkCvy7F5fcp2zZ8L',
     },
     {
         title: "Sweet Dreams (Are Made of This) - Remastered",
         artist: "Eurythmics, Annie Lennox, Dave Stewart",
         album: "Sweet Dreams (Are Made of This)",
         id: generateId(),
+        uri: 'spotify:track:1TfqLAPs4K3s2rJMoCokcS',
     },
     {
         title: "You Make My Dreams (Come True)",
         artist: "Daryl Hall & John Oates",
         album: "Voices",
         id: generateId(),
+        uri: 'spotify:track:4o6BgsqLIBViaGVbx5rbRk',
     },
     {
         title: "I Hate Myself for Loving You",
         artist: "Joan Jett & the Blackhearts",
         album: "Up Your Alley",
         id: generateId(),
+        uri: 'spotify:track:2HXixVqzzm9rEUIMAWzshl',
     },
     {
         title: "Should I Stay or Should I Go - Remastered",
         artist: "The Clash",
         album: "Combat Rock (Remastered)",
         id: generateId(),
+        uri: 'spotify:track:39shmbIHICJ2Wxnk1fPSdz',
     },
     {
         title: "All My Heros - Radio Edit",
         artist: "Naeleck, Sarah Rebecca",
         album: "All My Heroes",
         id: generateId(),
+        uri: 'spotify:track:0TRmtiR9q6XnYDzXmoMpGC',
     },
 ]
 
