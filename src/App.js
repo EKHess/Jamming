86d2b07e5df11e020c2af5bb2711347d7af1e554
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-03-08 16:19:19 -0700

    App now fetches a user's access token from Spotify, which we'll use later to interact with Spotify API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import './App.css';
 import SearchBar from "./components/SearchBar";
 import PlaylistForm from "./components/PlaylistForm";
 import PlaylistTrack from './components/PlaylistTrack';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import tracks from './SongsData';
 import TrackResult from './components/TrackResult';
 
+// Information for Spotify API
+const CLIENT_ID = 'a3c530b21aa84e3aa8da93ed77e04ce2';
+const CLIENT_SECRET = '5c54528389a9445e8fa2a29323c1de52';
+
 
 function App() {
+  const [accessToken, setAccessToken] = useState('');
+
+  // GET USER'S ACCESS TOKEN 
+  // The following code only runs the first time App renders.
+  // The following is a translation of the cURL code provided in the Spotify API Documentation
+  // here: https://developer.spotify.com/documentation/web-api/tutorials/getting-started
+  useEffect(() => {
+    // API Access Token
+    const fetchParams = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET,
+    }
+    fetch('https://accounts.spotify.com/api/token', fetchParams)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+  }, [])
 
   // Setup state for playlist, which will be updated as user interacts with App
   const [playlist, setPlaylist] = useState([]);
