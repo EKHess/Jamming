9ae1fcc2dbbb6765a29781cb3a3799238a1afcdf
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-03-15 11:02:25 -0600

    Added checkAuth function to extract the access_token returned by spotify in the URL after user agrees to using our App

--- src/utilities/Spotify.js
+++ src/utilities/Spotify.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = 'a3c530b21aa84e3aa8da93ed77e04ce2';
-const REDIRECT_URI = 'https://localhost:3000/';
+const REDIRECT_URI = 'http://localhost:3000/';
 
 let accessToken;
 let userID;
@@ -10,7 +10,22 @@ const Spotify = {
     getAuth() {
         const tokenUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
         window.location.href = tokenUrl;
-    }
+    },
+
+
+    checkAuth() {
+        // If /access_token= is in the URL, extract it. If you'd like an explainer, ask ChatGPT to explain the window.location.href.match(/access_token=([^&]*)/) part :-)
+        const authenticated = window.location.href.match(/access_token=([^&]*)/);  // Returns array, access_token at index 1
+        if (authenticated) {
+            accessToken = authenticated[1];
+            alert(`Successfully found access token for this user: ${accessToken}`);
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+
 }
 
 export default Spotify;
